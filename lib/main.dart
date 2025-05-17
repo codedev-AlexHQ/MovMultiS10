dcec19c46ee91e8916acc5ae76e9a7201b6cad38
codedev-AlexHQ <andresalexanderhq@gmail.com>
2025-05-17 11:04:19 -0500

    second commit

--- lib/main.dart
+++ lib/main.dart
@@ -7,6 +7,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
+        backgroundColor: Colors.blue,
         appBar: AppBar(
           title: Text('Cartelera de Cine'),
         ),
@@ -15,8 +16,7 @@ class MyApp extends StatelessWidget {
             BannerCard(),
             Expanded(child: MovieList()),
           ],
-        )
-
+        ),
       ),
     );
   }
@@ -24,26 +24,25 @@ class MyApp extends StatelessWidget {
 
 class MovieList extends StatelessWidget {
   final List<Movie> movies = [
-  Movie(
-    title: 'Indiana Jones y el dial del destino',
-    genre: 'Aventura',
-    description: 'Indiana Jones se embarca en una última aventura para recuperar un artefacto legendario.',
-    imageUrl: 'https://lumiere-a.akamaihd.net/v1/images/02_plt_payoff_4x5_arg_484b4a65.jpeg',
-  ),
-  Movie(
-    title: 'Deadpool y Wolverine',
-    genre: 'Acción/Comedia',
-    description: 'Deadpool se une a Wolverine en una misión irreverente y llena de acción.',
-    imageUrl: 'https://lumiere-a.akamaihd.net/v1/images/tidalwave_payoff_poster_las_0a47c6a2.jpeg',
-  ),
-  Movie(
-    title: 'Cruella',
-    genre: 'Drama/Comedia',
-    description: 'Cruella de Vil revela su historia en el mundo de la moda londinense.',
-    imageUrl: 'https://lumiere-a.akamaihd.net/v1/images/outfit_domestic_teaser2_las_94429e25.jpeg',
-  ),
-];
-
+    Movie(
+      title: 'Indiana Jones y el dial del destino',
+      genre: 'Aventura',
+      description: 'Indiana Jones se embarca en una última aventura para recuperar un artefacto legendario.',
+      imageUrl: 'https://lumiere-a.akamaihd.net/v1/images/02_plt_payoff_4x5_arg_484b4a65.jpeg',
+    ),
+    Movie(
+      title: 'Deadpool y Wolverine',
+      genre: 'Acción/Comedia',
+      description: 'Deadpool se une a Wolverine en una misión irreverente y llena de acción.',
+      imageUrl: 'https://lumiere-a.akamaihd.net/v1/images/tidalwave_payoff_poster_las_0a47c6a2.jpeg',
+    ),
+    Movie(
+      title: 'Cruella',
+      genre: 'Drama/Comedia',
+      description: 'Cruella de Vil revela su historia en el mundo de la moda londinense.',
+      imageUrl: 'https://lumiere-a.akamaihd.net/v1/images/outfit_domestic_teaser2_las_94429e25.jpeg',
+    ),
+  ];
 
   @override
   Widget build(BuildContext context) {
@@ -74,6 +73,7 @@ class BannerCard extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Card(
+      color: Colors.purple[800],
       margin: EdgeInsets.all(12),
       elevation: 4,
       shape: RoundedRectangleBorder(
@@ -92,45 +92,41 @@ class BannerCard extends StatelessWidget {
   }
 }
 
-
 class MovieCard extends StatelessWidget {
   final Movie movie;
   final int index;
 
-  MovieCard({ required this.movie, required this.index });
+  MovieCard({required this.movie, required this.index});
 
   static const fonts = ['Jacquard', 'Roboto2', 'Alegreya'];
 
   @override
   Widget build(BuildContext context) {
     final family = fonts[index % fonts.length];
-
     return Center(
       child: SizedBox(
         width: MediaQuery.of(context).size.width * 0.65,
         child: Card(
+          color: Colors.purple[700],
           elevation: 4,
-          margin: EdgeInsets.symmetric(vertical: 10),
+          margin: EdgeInsets.symmetric(vertical: 12),
           shape: RoundedRectangleBorder(
-            borderRadius: BorderRadius.circular(12),
+            borderRadius: BorderRadius.circular(14),
           ),
           child: Row(
             children: [
-              // Imagen a la izquierda
               ClipRRect(
-                borderRadius: BorderRadius.horizontal(left: Radius.circular(12)),
+                borderRadius: BorderRadius.horizontal(left: Radius.circular(14)),
                 child: Image.network(
                   movie.imageUrl,
-                  height: 180,
-                  width: 100,
+                  height: 200,
+                  width: 200,
                   fit: BoxFit.contain,
                 ),
               ),
-
-              // Texto a la derecha
               Expanded(
                 child: Padding(
-                  padding: EdgeInsets.all(12),
+                  padding: EdgeInsets.all(16),
                   child: Column(
                     mainAxisAlignment: MainAxisAlignment.center,
                     children: [
@@ -138,28 +134,30 @@ class MovieCard extends StatelessWidget {
                         movie.title,
                         textAlign: TextAlign.center,
                         style: TextStyle(
+                          color: Colors.white,
                           fontFamily: family,
-                          fontSize: 18,
+                          fontSize: 22,
                           fontWeight: FontWeight.bold,
                         ),
                       ),
-                      SizedBox(height: 6),
+                      SizedBox(height: 8),
                       Text(
                         movie.description,
                         textAlign: TextAlign.center,
                         style: TextStyle(
+                          color: Colors.white70,
                           fontFamily: family,
-                          fontSize: 14,
+                          fontSize: 18,
                         ),
                       ),
-                      SizedBox(height: 6),
+                      SizedBox(height: 8),
                       Text(
                         movie.genre,
                         textAlign: TextAlign.center,
                         style: TextStyle(
+                          color: Colors.white60,
                           fontFamily: family,
-                          fontSize: 16,
-                          color: Colors.grey[600],
+                          fontSize: 20,
                         ),
                       ),
                     ],
